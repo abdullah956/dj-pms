42b70b846d4069a0283a46e01848454daef94986
Abdullah <abdullaharshed956@gmail.com>
2024-06-11 15:45:08 +0500

    based model

--- users/models.py
+++ users/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .managers import UserManager
+from config.models import BasedModel
 
-class User(AbstractUser):
+class User(AbstractUser,BasedModel):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=150, blank=True, null=True)
 
