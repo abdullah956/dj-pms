d24969fdf28f02e669a69e19ab7472b406df2486
Abdullah <abdullaharshed956@gmail.com>
2024-06-11 16:10:44 +0500

    lint

--- users/models.py
+++ users/models.py
@@ -14,8 +14,8 @@ class User(AbstractUser, BasedModel):
     email = models.EmailField(unique=True)
     username = models.CharField(max_length=150, blank=True, null=True)
     role=models.PositiveSmallIntegerField(choices=USER_ROLES, null=True, blank=True)
-    USERNAME_FIELD='email'
-    REQUIRED_FIELDS=[]
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
 
     objects = UserManager()
 
