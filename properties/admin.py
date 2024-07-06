85c7980317962ef9d814fb353d5c75d68e698b6c
abdullah956 <abdullaharshed956@gmail.com>
2024-07-06 19:59:53 +0500

    feature rent price

--- properties/admin.py
+++ properties/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import City, Country, Property, State, SubLocality, Unit
+from .models import City, Country, Currency, Property, State, SubLocality, Unit
 
 admin.site.register(Country)
 admin.site.register(State)
@@ -8,3 +8,4 @@ admin.site.register(City)
 admin.site.register(SubLocality)
 admin.site.register(Property)
 admin.site.register(Unit)
+admin.site.register(Currency)
