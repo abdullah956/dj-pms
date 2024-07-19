0219f33f528d695d53eb2f336902a7ea4d637a2e
abdullah956 <abdullaharshed956@gmail.com>
2024-07-19 20:17:31 +0500

    upload docs for approval

--- properties/admin.py
+++ properties/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import City, Country, Currency, Property, State, SubLocality, Unit
+from .models import (City, Country, Currency, Document, Property, State,
+                     SubLocality, Unit)
 
 admin.site.register(Country)
 admin.site.register(State)
@@ -9,3 +10,4 @@ admin.site.register(SubLocality)
 admin.site.register(Property)
 admin.site.register(Unit)
 admin.site.register(Currency)
+admin.site.register(Document)
